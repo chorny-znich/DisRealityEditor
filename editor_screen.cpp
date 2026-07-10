366b4d3afa61725dc33fc39a94afeeee0d9230fc
chorny-znich <vargar.dt@gmail.com>
2026-07-10 17:01:32 +0300

    feat: start working with editors

--- editor_screen.cpp
+++ editor_screen.cpp
@@ -1,8 +1,20 @@
 #include "editor_screen.h"
 #include <imgui.h>
 #include <imgui-SFML.h>
+#include <map>
+#include <string>
 
-#include "about_screen.h"
+/**
+ * @brief Common structure for all "flat" ini editors
+ */
+struct EditorScreen::FlatIniEditor
+{
+	std::map<std::string, std::string> data;
+	std::string selectedKey{};
+	bool isEditing{ false };
+	char bufferKey[128] = "";
+	char bufferValue[1024] = "";
+};
 
 /**
  * @brief For handling Events in handleInput method
@@ -37,6 +49,13 @@ struct EditorScreen::ScreenInputVisitor
 	void operator()(const auto&) {};
 };
 
+EditorScreen::EditorScreen() :
+	mStringEditor(std::make_unique<FlatIniEditor>())
+{
+}
+
+EditorScreen::~EditorScreen() = default;
+
 /**
  * @brief Initialize screen resources
  */
@@ -47,16 +66,53 @@ void EditorScreen::init()
 
 void EditorScreen::handleInput(const sf::Event& event, sf::RenderWindow& window)
 {
+	ImGui::SFML::ProcessEvent(window, event);
 	event.visit(ScreenInputVisitor{ *this, window });
 }
 
 /**
- * @brief 
+ * @brief draw ImGui interface
  * @param dt 
  */
 void EditorScreen::update(float dt)
 {
 	ImGui::SFML::Update(dr::ImguiHelper::getWindow(), dr::ImguiHelper::getTime());
+
+	// Draw ImGui menus
+
+	if (ImGui::BeginMainMenuBar())
+	{
+		if (ImGui::BeginMenu("File"))
+		{
+			if (ImGui::MenuItem("Exit", "Esc"))
+			{
+				mExitEditor = true;
+			}
+			ImGui::EndMenu();
+		}
+
+		if (ImGui::BeginMenu("Editors"))
+		{
+			if (ImGui::MenuItem("Strings"))
+			{
+				mShowStringEditor = true;
+				mStringEditor->data.clear();
+
+			}
+			ImGui::EndMenu();
+		}
+		ImGui::EndMainMenuBar();
+	}
+
+	// Realisation
+	if (mExitEditor)
+	{
+		dr::ScreenManager::destroyScreen();
+	}
+	if (mShowStringEditor)
+	{
+		drawFlatIniEditor("String Editor", *mStringEditor);
+	}
 }
 
 /**
@@ -67,4 +123,20 @@ void EditorScreen::render(sf::RenderWindow& window)
 {
 	window.setView(mMainView);
 	ImGui::SFML::Render(window);
+}
+
+void EditorScreen::drawFlatIniEditor(const std::string& title, FlatIniEditor& editor)
+{
+	// Editor title
+	ImGui::TextColored(ImVec4(0.2f, 0.8f, 0.2f, 1.0f), title.c_str());
+	ImGui::Spacing();
+	// Read only table
+	if (ImGui::BeginTable("flatEditorTable", 2, ImGuiTableFlags_Borders | ImGuiTableFlags_RowBg |
+		ImGuiTableFlags_ScrollY, ImVec2(0.f, 300.f)))
+	{
+		ImGui::TableSetupColumn("ID");
+		ImGui::TableSetupColumn("Value");
+		ImGui::TableHeadersRow();
+		ImGui::EndTable();
+	}
 }
--- editor_screen.h
+++ editor_screen.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "game_data.h"
 #include <disreality_engine.h>
+#include <memory>
 
 /**
  * @brief 
@@ -8,12 +9,21 @@
 class EditorScreen : public dr::Screen
 {
 public:
+	EditorScreen();
+	~EditorScreen();
 	void init() override;
 	void handleInput(const sf::Event& event, sf::RenderWindow& window) override;
 	void update(float dt) override;
 	void render(sf::RenderWindow& window) override;
 private:
 	struct ScreenInputVisitor;
+	// Flat (structure) ini editors as strings, pathes to textures
+	struct FlatIniEditor;
+	std::unique_ptr<FlatIniEditor> mStringEditor;
+	void drawFlatIniEditor(const std::string& title, FlatIniEditor& editor);
 
 	sf::View mMainView{ sf::FloatRect({0, 0}, {gd::GraphicsResolution.x, gd::GraphicsResolution.y}) };
+
+	bool mExitEditor{ false };
+	bool mShowStringEditor{ false };
 };
